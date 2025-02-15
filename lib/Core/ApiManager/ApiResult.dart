23c6fae91a0879f09c38f29f4cee4522fd2155c1
Jimmybasha <gemiahmed2002@gmail.com>
2025-02-15 23:15:52 +0200

    +Resolve Main Conflict

---
+++ lib/Core/ApiManager/ApiResult.dart
@@ -0,0 +1,14 @@
+sealed class ApiResult<T>{}
+
+class SuccessApiResult<T> extends ApiResult<T>{
+  T? data;
+  SuccessApiResult(this.data);
+}
+
+class ErrorApiResult<T> extends ApiResult<T>{
+  Exception exception;
+  ErrorApiResult(this.exception);
+}
+
+class StreamApiResult extends ApiResult{
+}
