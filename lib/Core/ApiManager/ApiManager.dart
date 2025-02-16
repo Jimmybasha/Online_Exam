2457a8707e36e9031463fc8049f9bf88c20f1a74
Samah <Samahosama112@icloud.com>
2025-02-16 14:11:30 +0200

    Create ApiManager file to handle all api calls

--- lib/Core/ApiManager/ApiManager.dart
+++ lib/Core/ApiManager/ApiManager.dart
@@ -1,3 +1,52 @@
-class ApiManager{
+import 'package:dio/dio.dart';
+import 'package:dartz/dartz.dart';
+import 'package:injectable/injectable.dart';
+import 'package:online_exam/Core/Errors/Failure.dart';
+@Singleton()
+@injectable
+class ApiManager {
+  final Dio dio = Dio(
+    BaseOptions(
+      baseUrl: 'https://exam.elevateegy.com/api/v1/',
+      headers: {'Content-Type': 'application/json'},
+    ),
+  );
 
+  Future<Either<Failure, Response>> getRequest(String endpoint) async {
+    try {
+      final response = await dio.get(endpoint);
+      return Right(response.data);
+    } catch (e) {
+      return Left(ServerFailure(errorMessage: e.toString()));
+    }
+  }
+
+  Future<Either<Failure, Response>> postRequest(
+      String endpoint, Map<String, dynamic> data) async {
+    try {
+      final response = await dio.post(endpoint, data: data);
+      return Right(response.data);
+    } catch (e) {
+      return Left(ServerFailure(errorMessage: e.toString()));
+    }
+  }
+
+  Future<Either<Failure, Response>> putRequest(
+      String endpoint, Map<String, dynamic> data) async {
+    try {
+      final response = await dio.put(endpoint, data: data);
+      return Right(response.data);
+    } catch (e) {
+      return Left(ServerFailure(errorMessage: e.toString()));
+    }
+  }
+
+  Future<Either<Failure, dynamic>> deleteRequest(String endpoint) async {
+    try {
+      final response = await dio.delete(endpoint);
+      return Right(response.data);
+    } catch (e) {
+      return Left(ServerFailure(errorMessage: e.toString()));
+    }
+  }
 }
