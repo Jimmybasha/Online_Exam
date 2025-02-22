fb8ce18f2477d66e5440eb9127a70936bff52ee9
Samah <Samahosama112@icloud.com>
2025-02-22 22:25:55 +0200

    create show error snackbar and show snackbar

--- lib/Core/widgets/custom_button.dart
+++ lib/Core/widgets/custom_button.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:online_exam/Core/Constants/app_colors.dart';
-import 'package:online_exam/Core/Constants/app_text_style.dart';
 
 class CustomButton extends StatelessWidget {
   const CustomButton(
