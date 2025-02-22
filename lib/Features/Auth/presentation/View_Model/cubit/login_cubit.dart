5c9cc1c148035273a6c7d6100c27f49f80b0e9b2
Samah <Samahosama112@icloud.com>
2025-02-22 14:18:37 +0200

    create Login cubit

---
+++ lib/Features/Auth/presentation/View_Model/cubit/login_cubit.dart
@@ -0,0 +1,22 @@
+
+import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:online_exam/Features/Auth/data/Models/user_model.dart';
+import 'package:online_exam/Features/Auth/domain/UseCase/LoginUseCase.dart';
+
+part 'login_state.dart';
+
+class LoginCubit extends Cubit<LoginState> {
+  LoginCubit(this.loginUseCase) : super(LoginInitial());
+  final LoginUseCase loginUseCase;
+  Future<void> loginUser(
+      {required String email, required String password}) async {
+    emit(LoginLoading());
+    var result = await loginUseCase.call(email: email, password: password);
+    result.fold((failure) {
+      emit(LoginFailure(errorMessage: failure.errorMessage));
+    }, (user) {
+      emit(LoginSuccess(userModel: user));
+    });
+  }
+}
